1d9510f52376dc2d8db7b67e7f550cad4ef326b8
ysthakur <45539777+ysthakur@users.noreply.github.com>
2022-04-15 15:40:33 -0400

    Add skeleton

---
+++ src/main/java/frc/team449/Robot.java
@@ -0,0 +1,56 @@
+package frc.team449;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * The main class of the robot, constructs all the subsystems and initializes default commands.
+ */
+public class Robot extends TimedRobot {
+  @Override
+  public void robotInit() {
+  }
+
+  @Override
+  public void robotPeriodic() {
+  }
+
+  @Override
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  }
+
+  @Override
+  public void disabledInit() {
+  }
+
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  @Override
+  public void testInit() {
+  }
+
+  @Override
+  public void testPeriodic() {
+  }
+
+  @Override
+  public void simulationInit() {
+  }
+
+  @Override
+  public void simulationPeriodic() {
+  }
+}
