cac7c7f7181e82e7f3b5b16ddf222501500e60b5
ysthakur <2004yst@gmail.com>
2022-04-20 11:54:43 +0000

    Add clamping for OISwerve

--- src/main/java/frc/team449/abstractions/tank/TankDrive.java
+++ src/main/java/frc/team449/abstractions/tank/TankDrive.java
@@ -1,12 +1,13 @@
 package frc.team449.abstractions.tank;
 
+import org.jetbrains.annotations.NotNull;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
-import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import frc.team449.abstractions.DriveSubsystem;
 import frc.team449.system.AHRS;
 import frc.team449.system.motor.WrappedMotor;
@@ -19,22 +20,35 @@ public class TankDrive implements DriveSubsystem {
   /** The gyro used for the robot */
   private final AHRS ahrs;
   /**
-   * the kinematics used to convert {@link DifferentialDriveWheelSpeeds} to {@link ChassisSpeeds}
+   * the kinematics used to convert {@link ChassisSpeeds} to
+   * {@link DifferentialDriveWheelSpeeds}
    */
-  private final DifferentialDriveKinematics driveKinematics;
-  /** odometer that keeps track of where the robot is */
+  private final DifferentialDriveKinematics kinematics;
+  /** Odometry to keeps track of where the robot is */
   private final DifferentialDriveOdometry odometry;
-  /** Feedforward calculator */
+  /** Feedforward calculator for both sides */
   public final SimpleMotorFeedforward feedforward;
   /** Velocity PID controller for left side */
-  public final PIDController leftVelPID;
+  public final PIDController leftPID;
   /** Velocity PID controller for right side */
-  public final PIDController rightVelPID;
-  /** The track width of the robot/distance between left and right wheels in meters */
-  public final double trackWidth;
+  public final PIDController rightPID;
 
-  public TankDrive(WrappedMotor leftLeader, WrappedMotor rightLeader, AHRS ahrs) { // TODO
-    driveKinematics = new DifferentialDriveKinematics();
+  public TankDrive(
+      @NotNull WrappedMotor leftLeader,
+      @NotNull WrappedMotor rightLeader,
+      @NotNull AHRS ahrs,
+      @NotNull SimpleMotorFeedforward feedforward,
+      @NotNull PIDController leftPID,
+      @NotNull PIDController rightPID,
+      double trackwidth) {
+    this.leftLeader = leftLeader;
+    this.rightLeader = rightLeader;
+    this.ahrs = ahrs;
+    this.feedforward = feedforward;
+    this.leftPID = leftPID;
+    this.rightPID = rightPID;
+    this.kinematics = new DifferentialDriveKinematics(trackwidth);
+    this.odometry = new DifferentialDriveOdometry(ahrs.getHeading());
   }
 
   @Override
