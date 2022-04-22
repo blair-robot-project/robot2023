c67f6d32976770dc9f5d796a1e374bc2d9112fd3
John Joseph <jpothen8@gmail.com>
2022-04-21 22:45:20 -0400

    updated SwerveDriveSim

--- src/main/java/frc/team449/control/holonomic/SwerveDriveSim.java
+++ src/main/java/frc/team449/control/holonomic/SwerveDriveSim.java
@@ -4,22 +4,21 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import frc.team449.system.AHRS;
 
 public class SwerveDriveSim extends SwerveDrive implements HolonomicDrive {
-    private Rotation2d heading;
+    private double heading;
     private final double simUpdateInterval = 0.02;
 
     public SwerveDriveSim(AHRS ahrs, double maxLinearSpeed, double maxRotSpeed, Rotation2d heading, SwerveModule... modules) {
         super(ahrs, maxLinearSpeed, maxRotSpeed, modules);
-        this.heading = heading;
     }
 
     @Override
     public Rotation2d getHeading() {
-        return this.heading;
+        return new Rotation2d(this.heading);
     }
 
     @Override
     public void periodic() {
-        this.heading = Rotation2d.fromDegrees(simUpdateInterval * desiredSpeeds.omegaRadiansPerSecond);
+        this.heading += simUpdateInterval * desiredSpeeds.omegaRadiansPerSecond;
     }
 
 }
