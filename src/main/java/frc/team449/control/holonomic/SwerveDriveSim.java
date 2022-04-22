f5e5b6e8f6dfb858cb5ea83fcf4319e35534bc95
John Joseph <jpothen8@gmail.com>
2022-04-21 21:30:35 -0400

    heading updates in sim

--- src/main/java/frc/team449/control/holonomic/SwerveDriveSim.java
+++ src/main/java/frc/team449/control/holonomic/SwerveDriveSim.java
@@ -1,21 +1,25 @@
 package frc.team449.control.holonomic;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import frc.team449.system.AHRS;
+
 public class SwerveDriveSim extends SwerveDrive implements HolonomicDrive {
-    private @NotNull Rotation2d heading;
+    private Rotation2d heading;
     private final double simUpdateInterval = 0.02;
 
-    public SwerveDriveSim(@NotNull heading, desiredSpeeds) {
+    public SwerveDriveSim(AHRS ahrs, double maxLinearSpeed, double maxRotSpeed, Rotation2d heading, SwerveModule... modules) {
+        super(ahrs, maxLinearSpeed, maxRotSpeed, modules);
         this.heading = heading;
     }
 
     @Override
-    public double getHeading() {
+    public Rotation2d getHeading() {
         return this.heading;
     }
 
     @Override
     public void periodic() {
-        this.heading = simUpdateInterval * desiredSpeeds.omegaRadiansPerSecond;
+        this.heading = Rotation2d.fromDegrees(simUpdateInterval * desiredSpeeds.omegaRadiansPerSecond);
     }
 
 }
