098e7a9b1eecb45900175e7cafd8e68c739174b3
Tewodros Mengistie <Tewodros.mengistie@gmail.com>
2022-07-08 09:17:32 -0400

    Auto structure is done, add autoSequence() into AutoUtils.kt
    (cherry picked from commit ce10d9e224b602889eb27550d4d7296cedf6907c)

--- src/main/kotlin/frc/team449/control/auto/AutoUtils.kt
+++ src/main/kotlin/frc/team449/control/auto/AutoUtils.kt
@@ -2,7 +2,12 @@ package frc.team449.control.auto
 
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import edu.wpi.first.wpilibj2.command.WaitCommand
+import java.util.*
 import java.util.function.Function
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
 object AutoUtils {
   /**
@@ -39,6 +44,38 @@ object AutoUtils {
       }
       currStates.add(state)
     }
+    return cmd
+  }
+  /**
+   * @param timeStamp List of commands paired with desired time after beginning auto they are to be scheduled
+   *
+   */
+  fun autoSequence(
+    timeStamp: List<Pair<Double, Command>> // need a list of Time mapped to Commands to do in auto
+  ): SequentialCommandGroup {
+    val queue = PriorityQueue<Pair<Double, Command>>(
+      timeStamp.size
+    ) { o1, o2 ->
+      val a = o1.first
+      val b = o2.first
+      if (a >= b) if (a == b) 0 else -1
+      1
+    }
+    timeStamp.forEach { queue.offer(it) }
+    val cmd = SequentialCommandGroup()
+    var lastTime = 0.0
+    while (!queue.isEmpty()) {
+      val current = queue.poll()
+      println(current.first)
+      cmd.addCommands(
+        // wait command
+        WaitCommand(current.first - lastTime),
+        // actual command
+        current.second
+      )
+      lastTime = current.first
+    }
+    if (lastTime < 15.0) cmd.addCommands(WaitCommand(15.0 - lastTime))
     return cmd
   }
 }
