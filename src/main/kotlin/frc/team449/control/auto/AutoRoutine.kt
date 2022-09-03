5e7cf6a7954e4c01163afcf152c4c49cdef6f191
sirswagger21 <sirswagger21@gmail.com>
2022-09-03 19:09:36 -0400

    Removed traj from AutoRoutine

--- src/main/kotlin/frc/team449/control/auto/AutoRoutine.kt
+++ src/main/kotlin/frc/team449/control/auto/AutoRoutine.kt
@@ -3,4 +3,5 @@ package frc.team449.control.auto
 import edu.wpi.first.math.trajectory.Trajectory
 import edu.wpi.first.wpilibj2.command.Command
 
-class AutoRoutine(val name: String, val traj: Trajectory, val cmd: Command)
+class AutoRoutine(val name: String, val cmd: Command)
+// val traj1: (insert trajectory)
