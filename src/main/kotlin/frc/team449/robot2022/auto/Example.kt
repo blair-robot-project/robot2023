d08d32c71df9f7a901f4a30376d56e6059a30b3b
Tewodros Mengistie <Tewodros.mengistie@gmail.com>
2022-10-07 20:06:56 -0400

    satisfy klint

--- src/main/kotlin/frc/team449/robot2022/auto/Example.kt
+++ src/main/kotlin/frc/team449/robot2022/auto/Example.kt
@@ -2,7 +2,9 @@ package frc.team449.robot2022.auto
 
 import edu.wpi.first.wpilibj2.command.InstantCommand
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
-import frc.team449.control.auto.*
+import frc.team449.control.auto.AutoRoutine
+import frc.team449.control.auto.AutoUtils
+import frc.team449.control.auto.HolonomicFollower
 import frc.team449.robot2022.RobotContainer2022
 
 class Example(
