cc667b26ee6ecfc5d5c23ebfb8b9655b2f089c29
jpothen8 <jpothen8@gmail.com>
2023-02-06 10:51:40 -0500

    Make ktlint happy

--- src/main/kotlin/frc/team449/robot2023/commands/ProfiledPoseAlign.kt
+++ src/main/kotlin/frc/team449/robot2023/commands/ProfiledPoseAlign.kt
@@ -16,14 +16,7 @@ import frc.team449.control.holonomic.HolonomicDrive
  * @param headingPID The non-Profiled PID controller you want to use for fixing rotational error
  * @param tolerance The allowed tolerance from the targetPose
  */
-class ProfiledPoseAlign(
-  private val drive: HolonomicDrive,
-  private val targetPose: Pose2d,
-  private val xPID: ProfiledPIDController,
-  private val yPID: ProfiledPIDController,
-  private val headingPID: PIDController,
-  tolerance: Pose2d = Pose2d(0.05, 0.05, Rotation2d(0.05))
-) : CommandBase() {
+class ProfiledPoseAlign(private val drive: HolonomicDrive, private val targetPose: Pose2d, private val xPID: ProfiledPIDController, private val yPID: ProfiledPIDController, private val headingPID: PIDController, tolerance: Pose2d = Pose2d(0.05, 0.05, Rotation2d(0.05))) : CommandBase() {
   init {
     addRequirements(drive)
     headingPID.enableContinuousInput(-Math.PI, Math.PI)
@@ -33,13 +26,11 @@ class ProfiledPoseAlign(
     yPID.setTolerance(tolerance.y)
     headingPID.setTolerance(tolerance.rotation.radians)
 
-
     // Set the goals for all the PID controller to the target pose
     xPID.setGoal(targetPose.x)
     yPID.setGoal(targetPose.y)
     headingPID.setpoint = targetPose.rotation.radians
   }
-
 
   override fun execute() {
     // Calculate the feedback for X, Y, and theta using their respective controllers
@@ -47,12 +38,7 @@ class ProfiledPoseAlign(
     val yFeedback = yPID.calculate(drive.pose.y)
     val headingFeedback = headingPID.calculate(drive.pose.rotation.radians)
 
-    drive.set(ChassisSpeeds.fromFieldRelativeSpeeds(
-      xFeedback,
-      yFeedback,
-      headingFeedback,
-      drive.pose.rotation
-    ))
+    drive.set(ChassisSpeeds.fromFieldRelativeSpeeds(xFeedback, yFeedback, headingFeedback, drive.pose.rotation))
   }
 
   override fun isFinished(): Boolean {
