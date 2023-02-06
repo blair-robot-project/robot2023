beff2ac9050071e9b4e806f808562f6dbc239346
jpothen8 <jpothen8@gmail.com>
2023-02-06 10:54:02 -0500

    Try again

--- src/main/kotlin/frc/team449/robot2023/commands/ProfiledPoseAlign.kt
+++ src/main/kotlin/frc/team449/robot2023/commands/ProfiledPoseAlign.kt
@@ -16,7 +16,14 @@ import frc.team449.control.holonomic.HolonomicDrive
  * @param headingPID The non-Profiled PID controller you want to use for fixing rotational error
  * @param tolerance The allowed tolerance from the targetPose
  */
-class ProfiledPoseAlign(private val drive: HolonomicDrive, private val targetPose: Pose2d, private val xPID: ProfiledPIDController, private val yPID: ProfiledPIDController, private val headingPID: PIDController, tolerance: Pose2d = Pose2d(0.05, 0.05, Rotation2d(0.05))) : CommandBase() {
+class ProfiledPoseAlign(
+  private val drive: HolonomicDrive,
+  private val targetPose: Pose2d,
+  private val xPID: ProfiledPIDController,
+  private val yPID: ProfiledPIDController,
+  private val headingPID: PIDController,
+  tolerance: Pose2d = Pose2d(0.05, 0.05, Rotation2d(0.05))
+) : CommandBase() {
   init {
     addRequirements(drive)
     headingPID.enableContinuousInput(-Math.PI, Math.PI)
