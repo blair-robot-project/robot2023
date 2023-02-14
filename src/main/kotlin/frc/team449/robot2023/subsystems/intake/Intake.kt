bb0fbde6864f6de36ffa1f1b1cce62d3637f358b
Jeffery Ji <jefferydoudou@gmail.com>
2023-02-14 16:37:54 -0500

    integrated the IR sensor into the intake

--- src/main/kotlin/frc/team449/robot2023/subsystems/intake/Intake.kt
+++ src/main/kotlin/frc/team449/robot2023/subsystems/intake/Intake.kt
@@ -1,11 +1,16 @@
 package frc.team449.robot2023.subsystems.intake
 
+import edu.wpi.first.wpilibj.DigitalInput
 import edu.wpi.first.wpilibj.DoubleSolenoid
+import edu.wpi.first.wpilibj2.command.SubsystemBase
 
 class Intake(
-  private val intakePiston: DoubleSolenoid
-) {
+  private val intakePiston: DoubleSolenoid,
+  private val sensor: DigitalInput
+) : SubsystemBase() {
 
+  private var sensorVal = false
+  private var previousVal = false
   init {
     intakePiston.set(DoubleSolenoid.Value.kReverse)
   }
@@ -16,5 +21,13 @@ class Intake(
 
   fun pistonRev() {
     intakePiston.set(DoubleSolenoid.Value.kReverse)
+  }
+
+  override fun periodic() {
+    sensorVal = !sensor.get()
+    if (sensorVal != previousVal && sensorVal) {
+      pistonOn()
+    }
+    previousVal = sensorVal
   }
 }
