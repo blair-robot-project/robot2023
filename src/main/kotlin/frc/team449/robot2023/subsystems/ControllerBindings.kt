04d62bfc4ef8aeca28e73d852f7823d1f9d56d04
Teddy :] <Tewodros.mengistie@gmail.com>
2023-02-19 02:06:52 -0500

    add some sample trajectories and bind them

--- src/main/kotlin/frc/team449/robot2023/subsystems/ControllerBindings.kt
+++ src/main/kotlin/frc/team449/robot2023/subsystems/ControllerBindings.kt
@@ -1,7 +1,10 @@
 package frc.team449.robot2023.subsystems
 
 import edu.wpi.first.wpilibj.XboxController
+import edu.wpi.first.wpilibj2.command.button.JoystickButton
 import frc.team449.robot2023.Robot
+import frc.team449.robot2023.subsystems.arm.ArmPaths
+import frc.team449.robot2023.subsystems.arm.control.ArmFollower
 
 class ControllerBindings(
   private val controller: XboxController,
@@ -9,7 +12,7 @@ class ControllerBindings(
 ) {
 
   fun bindButtons() {
-//    /**  test trajectories */
+    /**  test trajectories */
 //    JoystickButton(controller, XboxController.Button.kA.value).onTrue(
 //      ArmFollower(robot.arm, ArmPaths.ZERO_STOW)
 //    )
@@ -17,5 +20,13 @@ class ControllerBindings(
 //    JoystickButton(controller, XboxController.Button.kB.value).onTrue(
 //      ArmFollower(robot.arm, ArmPaths.STOW_ZERO)
 //    )
+
+    JoystickButton(controller, XboxController.Button.kX.value).onTrue(
+      ArmFollower(robot.arm, ArmPaths.EXTEND_STOW)
+    )
+
+    JoystickButton(controller, XboxController.Button.kY.value).onTrue(
+      ArmFollower(robot.arm, ArmPaths.STOW_EXTEND)
+    )
   }
 }
