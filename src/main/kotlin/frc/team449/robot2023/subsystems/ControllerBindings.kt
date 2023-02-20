bec7875224fe11fa9fbc3680240fd2241bcde162
John Joseph <jpothen8@gmail.com>
2023-02-20 11:36:57 -0500

    Prepare for pull request (delete outreach code)

--- src/main/kotlin/frc/team449/robot2023/subsystems/ControllerBindings.kt
+++ src/main/kotlin/frc/team449/robot2023/subsystems/ControllerBindings.kt
@@ -1,9 +1,10 @@
 package frc.team449.robot2023.subsystems
 
+import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj.XboxController
-import edu.wpi.first.wpilibj2.command.InstantCommand
-import edu.wpi.first.wpilibj2.command.WaitCommand
 import edu.wpi.first.wpilibj2.command.button.JoystickButton
+import frc.team449.robot2023.commands.HeadingAlign
 
 class ControllerBindings(
   private val controller: XboxController,
@@ -11,28 +12,8 @@ class ControllerBindings(
 ) {
 
   fun bindButtons() {
-    // JoystickButton(controller, XboxController.Button.kX.value).onTrue(
-    // HeadingAlign(robot.drive, Translation2d(), PIDController(1.0, 0.0, 0.0))
-    // )
-
-    JoystickButton(controller, XboxController.Button.kA.value).onTrue(
-      InstantCommand(robot.intake::runIntakeForward).andThen(
-        InstantCommand(robot.shooter::runShooterReverse).andThen(
-          WaitCommand(0.5)
-        ).andThen(
-          InstantCommand(robot.shooter::runShooter)
-        )
-      )
-    ).onFalse(
-      InstantCommand(robot.shooter::stopShooter).andThen(
-        InstantCommand(robot.intake::stopIntake)
-      )
-    )
-
     JoystickButton(controller, XboxController.Button.kX.value).onTrue(
-      InstantCommand(robot.intake::runIntakeForward)
-    ).onFalse(
-      InstantCommand(robot.intake::stopIntake)
+      HeadingAlign(robot.drive, Translation2d(), PIDController(1.0, 0.0, 0.0))
     )
   }
 }
