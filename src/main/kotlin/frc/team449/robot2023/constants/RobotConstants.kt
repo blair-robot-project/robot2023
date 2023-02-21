cee85463f1e1630f07ea2726a375b8963a550c9a
Teddy :] <Tewodros.mengistie@gmail.com>
2023-02-21 08:11:54 -0500

    merge arm and auto testing

--- src/main/kotlin/frc/team449/robot2023/constants/RobotConstants.kt
+++ src/main/kotlin/frc/team449/robot2023/constants/RobotConstants.kt
@@ -1,7 +1,9 @@
 package frc.team449.robot2023.constants
 
+import edu.wpi.first.math.controller.PIDController
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.wpilibj.DriverStation
 
 object RobotConstants {
 
@@ -10,13 +12,18 @@ object RobotConstants {
   const val PCM_MODULE = 0
 
   /** Controller Configurations */
-  const val RATE_LIMIT = 1.5
+  const val RATE_LIMIT = 3.5
   const val TRANSLATION_DEADBAND = .125
   const val ROTATION_DEADBAND = .125
 
   /** Drive configuration */
-  const val MAX_LINEAR_SPEED = 1.5 // m/s
-  const val MAX_ROT_SPEED = 2.0 // rad/s
-  const val MAX_ACCEL = 5.0 // m/s/s
+  const val MAX_LINEAR_SPEED = .7 // m/s
+  const val MAX_ROT_SPEED = Math.PI // rad/s
+  const val MAX_ACCEL = 4.0 // m/s/s
   val INITIAL_POSE = Pose2d(0.0, 0.0, Rotation2d.fromDegrees(0.0))
+
+  /** PID controller for Orthogonal turning */
+  val ORTHOGONAL_CONRTOLLER = PIDController(1.0, 0.0, 0.0)
+
+  var ALLIANCE_COLOR: DriverStation.Alliance = DriverStation.getAlliance()
 }
