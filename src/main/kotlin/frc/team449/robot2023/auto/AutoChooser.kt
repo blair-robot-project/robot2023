b99917fa54ae8a85ee1912ba9612e088cb628e8d
John Joseph <jpothen8@gmail.com>
2023-02-25 15:06:49 -0500

    No wildcard import

--- src/main/kotlin/frc/team449/robot2023/auto/AutoChooser.kt
+++ src/main/kotlin/frc/team449/robot2023/auto/AutoChooser.kt
@@ -3,6 +3,15 @@ package frc.team449.robot2023.auto
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj2.command.Command
 import frc.team449.robot2023.Robot
+import frc.team449.robot2023.auto.routines.Example
+import frc.team449.robot2023.auto.routines.FarCone
+import frc.team449.robot2023.auto.routines.FarConeCube
+import frc.team449.robot2023.auto.routines.FarConeCubeStation
+import frc.team449.robot2023.auto.routines.FarConeStation
+import frc.team449.robot2023.auto.routines.WallCone
+import frc.team449.robot2023.auto.routines.WallConeCube
+import frc.team449.robot2023.auto.routines.WallConeCubeStation
+import frc.team449.robot2023.auto.routines.WallConeStation
 
 class AutoChooser(robot: Robot) : SendableChooser<Command>() {
   init {
