69548e245cc37ee543164d3005fcbe0a50f11990
Rakshay N <rakshay2108@gmail.com>
2023-03-02 11:18:46 -0500

    added roller class
    intake is already taken what should i name it?

---
+++ src/main/kotlin/frc/team449/robot2023/subsystems/roller/Roller.kt
@@ -0,0 +1,25 @@
+package frc.team449.robot2023.subsystems.roller
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase
+import frc.team449.robot2023.subsystems.arm.Arm
+import frc.team449.system.motor.WrappedMotor
+
+class Roller(
+  private val rollerMotor: WrappedMotor,
+  private val arm: Arm
+  ): SubsystemBase() {
+
+  fun runIntake() {
+    rollerMotor.setVoltage(RollerConstants.INTAKE_VOLTAGE)
+  }
+
+  fun runIntakeReverse() {
+    rollerMotor.setVoltage(-RollerConstants.INTAKE_VOLTAGE)
+  }
+
+  fun stop() {
+    rollerMotor.stopMotor()
+  }
+
+
+}
---
+++ src/main/kotlin/frc/team449/robot2023/subsystems/roller/RollerConstants.kt
@@ -0,0 +1,6 @@
+package frc.team449.robot2023.subsystems.roller
+
+object RollerConstants {
+
+  const val INTAKE_VOLTAGE = 7.0
+}
