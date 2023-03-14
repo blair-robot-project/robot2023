139c8a57c400882d9af890523d281705401f85e8
Jpothen8 <jpothen8@gmail.com>
2023-03-14 16:26:17 -0400

    code for new subsystems

--- src/main/kotlin/frc/team449/robot2023/constants/subsystem/EndEffectorConstants.kt
+++ src/main/kotlin/frc/team449/robot2023/constants/subsystem/EndEffectorConstants.kt
@@ -4,4 +4,8 @@ object EndEffectorConstants {
   const val FORWARD_CHANNEL = 5
   const val REVERSE_CHANNEL = 4
   const val SENSOR_CHANNEL = 10
+  // roller intake vals
+  const val CUBE_CHANNEL = 0
+  const val CONE_CHANNEL = 0
+  const val INTAKE_VOLTAGE = 3.5
 }
