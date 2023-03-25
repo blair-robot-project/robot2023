08bf1e57349096e8b04cc5e2daaa5d2c84578540
jpothen8 <jpothen8@gmail.com>
2023-03-25 06:06:44 -0400

    Fic negated arm sweep issue at high pos

--- src/main/kotlin/frc/team449/robot2023/commands/ArmSweep.kt
+++ src/main/kotlin/frc/team449/robot2023/commands/ArmSweep.kt
@@ -20,7 +20,7 @@ class ArmSweep(
 
   override fun initialize() {
     startState = arm.desiredState.copy()
-    sweepBeta = if (startState.beta.degrees <= 0.0) Rotation2d.fromDegrees(-abs(sweepBeta.degrees))
+    sweepBeta = if (startState.beta.degrees < 0.0) Rotation2d.fromDegrees(-abs(sweepBeta.degrees))
     else Rotation2d.fromDegrees(abs(sweepBeta.degrees))
   }
   override fun execute() {
