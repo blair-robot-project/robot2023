7656c2df556efb276d4c08fcc65a294e74f9f456
Teddy :] <Tewodros.mengistie@gmail.com>
2023-03-30 10:01:35 -0400

    delete old characterizer

--- src/main/kotlin/frc/team449/robot2023/subsystems/groundIntake/GroundIntake.kt
+++ src/main/kotlin/frc/team449/robot2023/subsystems/groundIntake/GroundIntake.kt
@@ -6,11 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.InstantCommand
 import edu.wpi.first.wpilibj2.command.SubsystemBase
 import edu.wpi.first.wpilibj2.command.WaitCommand
-import frc.team449.robot2023.Robot
-import frc.team449.robot2023.constants.subsystem.ArmConstants
 import frc.team449.robot2023.constants.subsystem.GroundIntakeConstants
-import frc.team449.robot2023.subsystems.arm.Arm
-import frc.team449.robot2023.subsystems.endEffector.EndEffector
 import frc.team449.system.encoder.NEOEncoder
 import frc.team449.system.motor.WrappedMotor
 import frc.team449.system.motor.createSparkMax
