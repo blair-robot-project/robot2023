21d94b2bf09acf683c4c4ced0283bf8b8eb98738
Rakshay N <rakshay2108@gmail.com>
2023-03-30 09:51:45 -0400

    initialized ground Intake for testing
    hi

--- src/main/kotlin/frc/team449/robot2023/constants/subsystem/GroundIntakeConstants.kt
+++ src/main/kotlin/frc/team449/robot2023/constants/subsystem/GroundIntakeConstants.kt
@@ -7,14 +7,17 @@ object GroundIntakeConstants {
   const val INTAKE_VOLTAGE = 3.5
 
   /** Ground Intake Motor Constants */
-  const val INTAKE_TOP = 20
-  const val INTAKE_BOTTOM = 21
+  const val INTAKE_TOP = 8
+  const val INTAKE_BOTTOM = 9
   const val UPR = 2 * PI
   const val GEARING = 1.0 / 3.0
   const val TOP_INVERTED = true
   const val BOTTOM_INVERTED = false
   const val CURRENT_LIM = 20
 
-  const val PISTON_FWD = 7
-  const val PISTON_REV = 0
+  const val PISTON_FWD_1 = 7
+  const val PISTON_REV_1 = 0
+
+  const val PISTON_FWD_2 = 6
+  const val PISTON_REV_2 = 1
 }
